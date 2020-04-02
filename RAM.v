abd5dacb9d545dcb768ddef609afb10d0fb968a3
nickdalfarra <dalfarra.nick@queensu.ca>
2020-04-02 12:24:52 -0400

    initialize ram contents at address 0

--- RAM.v
+++ RAM.v
@@ -2,6 +2,11 @@ module RAM (input read, write, inout [31:0] data, input [8:0] addr);
 
 	reg [31:0] mem [0:(1<<8)-1]; // word size 32, 2^9 words
 	
+		// whack shit but just pinch your nose and git er done
+	initial begin
+		mem[0] = 32'h04000055;
+	end
+	
 	assign data = (read == 1) ? mem[addr] : 32'bz;
 	
 	always @(*) begin
