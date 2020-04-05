03734e7ddd7aa14cbffe471c7edf83a212c06263
Jessica Landon <15jdl4@queensu.ca>
2020-04-05 09:59:42 -0400

    Testbenches for all instructions.

--- RAM.v
+++ RAM.v
@@ -4,13 +4,33 @@ module RAM (input read, write, inout [31:0] data, input [8:0] addr);
 	
 	assign data = (read == 1) ? mem[addr] : 32'bz;
 	
-	// Assign values in ram here
+	// Assign values in ram here, in further stages, would use .mif file
+	// Belatedly realized it would be much more efficient to assign each to a different address
+	// I'm in too deep now and out of time 
 	initial begin
-	   // For load
-		 mem[0] <= 'h00800055;
-		 mem[85] <= 'h00000002;
-		// For store
-		//mem[0] <= 'h1080005A;
+	   // For ld R1, $85
+		 //mem[0] <= 'h00800055;
+		 //mem[85] <= 'h00000002;
+		// For st $90, R1
+		//mem[0] <= 'h1080005A; 
+		// For st $90(R1), R1
+		//mem[0] <= 'h1088005A;
+		// For addi R2, R1, -5
+		//mem[0] <= 32'h590FFFFB;
+		// For andi/ori R2, R1, $26
+		//mem[0] <= 32'h5908001A;
+		// For jr R1
+		//mem[0] <= 32'h98800000;
+		// For jal R1
+		// mem[0] <= 32'hA0800000;
+		// For mfhi R1
+		 //mem[0] <= 32'hB8800000;
+		// For mflo R1
+		// mem[0] <= 32'hC0800000;
+		// For out R1
+		// mem[0] <= 32'hB0800000;
+		// For in R1
+		mem[0] <= 32'hA8800000;
 	end
 	
 	always @(*) begin
--- inout_instr_tb.v
+++ inout_instr_tb.v
@@ -1,18 +1,20 @@
-module st_instr_tb();
-   reg PCout, Zlowout, MDRout, Cout;
-   reg MARin, Zin, PCin, MDRin, IRin, Yin, Gra, Grb, Grc, Rin, Rout, BAout;
+module inout_instr_tb();
+   reg PCout, Zlowout, MDRout, Cout, HIout, LOout, InPortOut;
+   reg MARin, Zin, PCin, MDRin, IRin, Yin, Gra, Grb, Grc, Rin, Rout, BAout, HIin, LOin, OutPort, strobe;
    reg IncPC, Read, Write, ADD, AND, OR, SUB, SHR, SHL, ROL, ROR, NEG, NOT;
    reg Clock, clear;
+	reg [31:0] In_Data;
 	wire [31:0] Mdatain, ram_data, MDR, MAR, R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, 
-	R13, R14, R15, Hi, Lo, PC, bus_mux_out, IR, C_sign_ext;
+	R13, R14, R15, Hi, Lo, PC, bus_mux_out, IR, C_sign_ext, OutPortOut, InPortOutput;
 	wire [63:0] Z, ALUout;
    wire [15:0] Rins, Routs;
    
    parameter Default = 4'b0000, Reg_load_1a = 4'b0001, Reg_load_1b = 4'b0010, T0 = 4'b0011, T1 = 4'b0100,
-	T2 = 4'b0101, T3 = 4'b0110, T4 = 4'b0111, T5 = 4'b1000, T6 = 4'b1001, T7 = 4'b1010, T8 = 4'b1011;
+	T2 = 4'b0101, T3 = 4'b0110;
    reg [3:0]  Present_state = Default;
 
-   Datapath DUT (.write(Write), .Mdatain(Mdatain), .clear(clear), .Cout(Cout), .BAout(BAout), .PCout(PCout), .Zlowout(Zlowout), 
+   Datapath DUT (.InPortOutput(InPortOutput), .InPortIn(In_Data), .strobe(strobe), 
+	.Inportout(InPortOut), .OutPortOut(OutPortOut), .OutPort(OutPort),.HIin(HIin), .LOin(LOin), .HIout(HIout), .LOout(LOout),.write(Write), .Mdatain(Mdatain), .clear(clear), .Cout(Cout), .BAout(BAout), .PCout(PCout), .Zlowout(Zlowout), 
 	.MDRout(MDRout), .Gra(Gra), .Grb(Grb), .Grc(Grc), .Rin(Rin), .Rout(Rout), .MARin(MARin), 
 	.Zin(Zin), .PCin(PCin), .MDRin(MDRin), .IRin(IRin), .Yin(Yin), .IncPC(IncPC), .read(Read), 
 	.ADD(ADD), .clk(Clock), .MAR(MAR), .R0(R0), .R1(R1), .R2(R2), .R3(R3), .R4(R4), 
@@ -36,15 +38,10 @@ module st_instr_tb();
       case (Present_state)
 		Default : Present_state = Reg_load_1a;
 		Reg_load_1a : Present_state = Reg_load_1b;
-		Reg_load_1b : Present_state = T0;
+		Reg_load_1b : #5 Present_state = T0;
 		T0 : Present_state = T1;
-		T1 : #10 Present_state = T2;
-		T2 : #10 Present_state = T3;
-		T3 : #10 Present_state = T4;
-		T4 : #20 Present_state = T5;
-		T5:  #40 Present_state = T6;
-		T6 : #60 Present_state = T7;
-		T7 : #20 Present_state = T8;
+		T1 : #15 Present_state = T2;
+		T2 : #30 Present_state = T3;
       endcase // case (Present_state)      
    end // always @ (posedge Clock)
 
@@ -63,11 +60,16 @@ module st_instr_tb();
 	   Grb <= 0;
 	   Grc <= 0;	   
 	   MARin <= 0;
+		OutPort <= 0;
 	   Zin <= 0;
 	   PCin <= 0;
 	   MDRin <= 0;
 	   IRin <= 0;
 	   Yin <= 0;
+		HIin <= 0;
+		LOin <= 0;
+		HIout <= 0;
+		LOout <= 0;
 	   IncPC <= 0;
 	   Write <= 0;
 		Read <= 0;
@@ -82,9 +84,12 @@ module st_instr_tb();
 		NEG <= 0;
 		NOT <= 0;
 		BAout <= 0;
-	end // case: Default	
+		strobe <= 0;
+		InPortOut <= 0;
+	end // case: Default
+	// For out R1 instr:
 	// Load value of $85 into R1 from RAM
-	Reg_load_1a: begin
+	/* Reg_load_1a: begin
 		clear <= 0;
 		force ram_data = 'h00000055;
 	   #10 Read <= 1; MDRin <= 1;
@@ -92,83 +97,65 @@ module st_instr_tb();
 		release ram_data;
 	end
 	Reg_load_1b: begin
-		force Rins = 'h0002;
+	   force Rins = 'h0002;
 	   #10 MDRout <= 1;
 	   #15 MDRout <= 0;
 		release Rins;
+	end */
+	// For in R1 instr:
+	// Send $85 to InPort
+	Reg_load_1a: begin
+		clear <= 0;
+		In_Data <= 32'h00000055;
+		#10 strobe <= 1; 
+		#15 strobe <= 0;
 	end
-	// Get PC in MAR to go to mem and get st instr (@ addr 0), get PC+4 in Z
+	Reg_load_1b: begin
+	 // Do nothing; need so I can use 1 tb for out and in
+	end
+	// Get PC in MAR to go to mem and get instr (@ addr 0), get PC+4 in Z
 	T0: begin
 		PCout <= 1;
 	   MARin <= 1;
 	   IncPC <= 1;
 	   Zin <= 1;	   
 	end
-	// Put PC+4 in PC, load instr (st $90, R1) from memory into MDR
+	// Put PC+4 in PC, load mf instr from memory into MDR
 	T1: begin
-		PCout <= 0; MARin <= 0;
-	   IncPC <= 0;
-		#20 Zlowout <= 1;
+		#20 PCout <= 0; MARin <= 0;
+	   IncPC <= 0; Zlowout <= 1;
 	   PCin <= 1; Read <= 1; 
 	   MDRin <= 1;
 		Zin <= 0;
 	end
 	// Load instr into IR
 	T2: begin
-		#10 MDRin <= 0;
+		#15
+		MDRin <= 0;
 	   Zlowout <= 0;
 		PCin <= 0;
 	   Read <= 0;
 	   #10 MDRout <= 1;
 	   IRin <= 1;
 	end
-	// Put either 0's on bus if R0 selected or contents of Rb
-	// Put bus contents in Y
+	// For out R1 instr:
+	// Send R1 contents to Outport
+	/* T3: begin
+		#10 MDRout <= 0;
+	   IRin <= 0;
+	   #10 Gra <= 1;
+	   Rout <= 1;
+	   OutPort <= 1;
+	end */
+	// For in R1 instr:
+	// Send InPort data to R1
 	T3: begin
 		#10 MDRout <= 0;
 	   IRin <= 0;
-	   #10 Grb <= 1;
-	   BAout <= 1;
-	   Yin <= 1;
+	   #10 Gra <= 1;
+	   Rin <= 1;
+	   InPortOut <= 1;
 	end
-	// Put C_sign_ext = 32'h5A in Z
-	T4: begin
-	   #10 
-		BAout <= 0;
-	   Grb <= 0;
-	   Yin <= 0;
-		Cout <= 1;
-	   ADD <= 1;
-	   Zin <= 1; 
-	end
-	// Put C_sign_ext = 32'h5A in MAR
-	T5: begin	   
-	   #20 Cout <= 0;
-	   ADD <= 0;
-		Zin <= 0;
-		Zlowout <= 1;
-	   MARin <= 1;
-	   #20 Zlowout <= 0;
-	   MARin <= 0;
-	end
-	// Ignore this phase :(
-	T6: begin
-		// For some reason any assignments i do in this phase don't happen
-		// I think it's being skipped due to previous # delays...to fix
-	end
-	// Take data in R1 ($85 = 'h55) and write to mem
-	T7: begin
-		Rout <= 1;
-		Gra <= 1;
-	   MDRin <= 1;
-		Write <= 1;
-	end
-	// Read back memory at address $90 ('h5A), should be $85
-	// Will be in MDR
-	T8 : begin
-		#20
-		Write <= 0; Read <= 1; MDRin <= 1;
-	end
-      endcase // case (Present_state)
-   end // always @ (Present_state)   
+  endcase // case (Present_state)
+ end // always @ (Present_state)   
 endmodule
